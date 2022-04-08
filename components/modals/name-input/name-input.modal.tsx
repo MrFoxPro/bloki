ad0638e0edc79702299cf8030f642b3f5f8fda7a
Dmitriy Nikiforov <tuningiposadka@gmail.com>
2022-04-08 16:50:51 +0500

    increase name length

--- components/modals/name-input/name-input.modal.tsx
+++ components/modals/name-input/name-input.modal.tsx
@@ -4,17 +4,18 @@ import s from './name-input.module.scss';
 import { randAnimalType } from '@ngneat/falso';
 import { upperFirst } from "@/lib/helpers";
 
+const NAME_MAX_LENGTH = 22;
 export function NameInput() {
    const [app, { setAppStore }] = useAppStore();
    const [t] = useI18n();
-   let name = upperFirst(randAnimalType({ length: 5 }).find(x => x.length < 18));
+   let name = upperFirst(randAnimalType({ length: 5 }).find(x => x.length < NAME_MAX_LENGTH));
    return (
       <div class={s.askName}>
          <div class={s.question}>{t('auth.ask-name.question')}</div>
          <input type="text" onInput={(e) => name = e.currentTarget.value} value={name} class={s.link} />
          <button
             onClick={() => {
-               if (name.length < 1 || name.length > 18) {
+               if (name.length < 1 || name.length > NAME_MAX_LENGTH) {
                   return alert('Name is too short/long!');
                }
                setAppStore({ name });
