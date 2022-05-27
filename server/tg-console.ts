642c7d2629fa810911434c5b4a60436cae65f7de
MrFoxPro <tuningiposadka@gmail.com>
2022-05-27 16:17:15 +0500

    Update tg-console.ts

--- server/tg-console.ts
+++ server/tg-console.ts
@@ -7,5 +7,5 @@ const bot = new Telegraf(process.env.TG_KEY);
 
 export const tg = (...args: string[]) => {
    if (process.env.MODE !== 'prod') return null;
-   return bot.telegram.sendMessage(process.env.TG_CHAT, util.format(`[${process.env.MODE ?? 'dev'}]: ${args[0]}`, ...args.slice(1)));
+   return bot.telegram.sendMessage(process.env.TG_CHANNEL, util.format(`[${process.env.MODE ?? 'dev'}]: ${args[0]}`, ...args.slice(1)));
 };
